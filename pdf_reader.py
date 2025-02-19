4cb288f763eaf9632ead8f51c54297f9f10d002e
Vítor <vitor.ass@hotmail.com>
2025-02-19 01:57:29 -0300

    Atualizando base textual, respostas melhoradas

--- pdf_reader.py
+++ pdf_reader.py
@@ -1,27 +1,24 @@
 import os
-from PyPDF2 import PdfReader
+import pdfplumber
 
-# Função para carregar e extrair texto de todos os PDFs de um diretório
-def load_pdfs_to_txt(directory, output_file):
+def load_pdfs_to_txt_with_plumber(directory, output_file):
     with open(output_file, "w", encoding="utf-8") as txt_file:
         for filename in os.listdir(directory):
             if filename.lower().endswith(".pdf"):
                 file_path = os.path.join(directory, filename)
                 print(f"Lendo: {filename}")
-                with open(file_path, "rb") as f:
-                    reader = PdfReader(f)
+                with pdfplumber.open(file_path) as pdf:
                     text = ""
-                    for page in reader.pages:
+                    for page in pdf.pages:
                         page_text = page.extract_text()
                         if page_text:
                             text += page_text + "\n"
                     txt_file.write(text)
-                    txt_file.write("\n\n")  # Separar conteúdos de diferentes PDFs
+                    txt_file.write("\n\n")
     print(f"Texto extraído e salvo em: {output_file}")
 
+
 if __name__ == "__main__":
-    # Diretório com os PDFs
-    pdf_directory = r"C:\Users\vitor\Downloads\LNViA-pdfs"  #TODO: Alterar para o caminho da sua máquina
-    # Caminho para o arquivo de saída
+    pdf_directory = r"C:\Users\vitor\OneDrive\Documentos\LNViA-pdfs_atualizados"
     output_txt_file = "base_textual.txt"
-    load_pdfs_to_txt(pdf_directory, output_txt_file)
+    load_pdfs_to_txt_with_plumber(pdf_directory, output_txt_file)
